a68cdc6907d0bebaad814ed11ef354e1dbcda4b0
Arslan <arslan@rebeltechnology.io>
2021-11-19 11:45:07 +0500

    J0-35 - working on it

--- src/component/FileUploader/FileUploader.tsx
+++ src/component/FileUploader/FileUploader.tsx
@@ -5,9 +5,14 @@ import './FileuploaderResponsive.scss';
 export const FileUploader: FC = () => {
     return (
         <>
-            <div id="file-uploader">
+            <div id="file-uploader" style={{ paddingTop: 100 }}>
                 <div className="container">
-                    abc
+                    <form action="">
+                        <label htmlFor="FileUploader">
+                            <input id="FileUploader" type="file"/>
+                            <span>Drag & drop your file here </span>
+                        </label>
+                    </form>
                 </div>
             </div>
         </>
